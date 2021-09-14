dd6e5da4f7c40a247b0a97545a2fae7546c3624f
Jaleen <jaleenbowenskelly@gmail.com>
2021-09-13 18:32:01 -0700

    Quick Update
    Just a simple update. After testing, I think a multidimensal array would work just fine

---
+++ src/UserSort.php
@@ -0,0 +1,13 @@
+<?php
+
+//This is actual code for sorting users in groups alphabetically and loads data about user
+
+$User_List = array  (
+    array("Username"),
+    array("Birth_Date"),
+    array("Location"),
+    array("Fav_Color")
+);
+
+
+?>
