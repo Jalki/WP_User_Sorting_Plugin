efa10a1ba1c938ce22fcc1230c93aaf28075b310
Jaleen <jaleenbowenskelly@gmail.com>
2021-09-15 10:54:59 -0700

    Do this instead
    Rather than create my own users, I will just use https://jsonplaceholder.typicode.com/users and import it into my php code

--- src/UserSort.php
+++ src/UserSort.php
@@ -2,11 +2,15 @@
 
 //This is actual code for sorting users in groups alphabetically and loads data about user
 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
 $User_List = array  (
     array("Username"),
     array("Birth_Date"),
     array("Location"),
-    array("Fav_Color")
+    array("ID")
 );
 
 
