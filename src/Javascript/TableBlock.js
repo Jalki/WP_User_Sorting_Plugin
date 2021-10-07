14bafdbe7f456b66fc9d0406f7c837ec7e4ad151
Jaleen <jaleenbowenskelly@gmail.com>
2021-10-07 09:49:32 -0400

    Update TableBlock.js

--- src/Javascript/TableBlock.js
+++ src/Javascript/TableBlock.js
@@ -2,10 +2,10 @@ wp.blocks.registerBlockType("ImpsydePlugin/User-Table", {
     title: "Table of Users",
     icon: "smiley",
     category: "common",
-    edit: function(){
+    edit: function(props){
         return wp.element.createElement("table", null, "Table");
     },
-    save: function(){
+    save: function(props){
         return wp.element.createElement("table", null, "This is Table for users");
     }
 })
