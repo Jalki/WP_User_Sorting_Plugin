d71d577d91b09f2fea4752cd6c4775659cc06a62
Jaleen <jaleenbowenskelly@gmail.com>
2021-10-16 16:04:52 -0400

    Update Alert.js

--- src/Javascript/Alert.js
+++ src/Javascript/Alert.js
@@ -1,18 +1,1 @@
-alert("Code for Custom Block");
-import { registerBlockType } from '@wordpress/blocks';
-
-wp.blocks.registerBlockType('ImpsydePlugin/User-Table', {
-    title: 'Table-of-Users',
-    icon: 'smiley',
-    category: 'common',
-    attributes: {
-        content:{type: 'string'},
-        color: {type: 'string'}
-    },
-    edit: function(props){
-        return wp.element.createElement('input', {type: "text"});
-    },
-    save: function(props){
-        return wp.element.createElement('table', null, 'This is Table for users');
-    }
-});
+alert("Code for Custom Block")
