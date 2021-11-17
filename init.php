e6cf5c96a0d0224c4f07c99ca4f066aaec1507eb
Jaleen <jaleenbowenskelly@gmail.com>
2021-11-17 17:27:58 -0500

    Maybe
    I updated the javascript to import what should be the lib for the custom block. If that doesnt work, then I need to make a register_block() function in init.php

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 
 /*This*/
-class ImpsydeCodeSetting{
+class CodeSettings{
     function __construct(){
         add_action('admin_menu', array($this, 'Settings') );
     }
@@ -14,6 +14,9 @@ class ImpsydeCodeSetting{
             <h1>Settings</h1>
         </div>
         <div>
+            <h2>Color</h2>
+        </div>
+        <div>
             <h2></h2>
         </div>
     <?php
@@ -21,44 +24,34 @@ class ImpsydeCodeSetting{
 }
 
 /*This class initialize the javascript that allows us to download the data we need and an test Alert when entering a post/page editing*/
-class Dataload{
-    public function __construct(){
-    add_action( 'enqueue_block_editor_assets', array($this, 'loadAlertjs'));
-    add_action( 'enqueue_block_editor_assets', array($this, 'loadDatajs'));
-    add_action('enqueue_block_editor_assets', array($this,'loadMyBlockFiles'));
+class Data
+{
+    function __construct()
+    {
+        add_action('enqueue_block_editor_assets', array($this, 'CustomBlock'));
     }
-    /*Fires an Javascript alert when loading in block editor on any posts or page!*/
-    function loadAlertjs(){
-        wp_enqueue_script( 
-            "Alert_User!",
-            plugin_dir_url(__FILE__) . "src/Javascript/Alert.js"  ,
-            array('wp-blocks'),
-            true
-        );
-    }
-    /*Loads Data.js*/
-    function loadDatajs(){
-        wp_enqueue_script( 
-            "Data",
-            plugin_dir_url(__FILE__) . "src/Javascript/Data.js",
+
+    function Data(){
+        wp_enqueue_script(
+            'Impsyde-Code-Test',
+            plugin_dir_url(__FILE__) . 'Tableblock.js',
             array('wp-blocks', 'wp-i18n', 'wp-editor'),
             true
-        );
+            );
     }
-    /*This initialize and creates the custom block type that will contain our table of users*/
-    function loadMyBlockFiles() {
-        wp_enqueue_script(
-        'Impsyde-Code-Test',
-        plugin_dir_url(__FILE__) . 'Tableblock.js',
-        array('wp-blocks', 'wp-i18n', 'wp-editor'),
-        true
-        );
-    }
+}
+
+class CustomBlock
+{
+    
 }
 
  
 /*Creates and init classes*/
 /**$ImpsydeClass = new ImpsydeCodeTest();*/
-$ImpsydeData = new Dataload();
-$ImpsydeSettingsClass = new ImpsydeCodeSetting();
+$Data = new Dataload();
+$SettingsClass = new CodeSettings();
 ?>
+
+
+
--- src/CustomBlock/CustomBlock.php
+++ src/CustomBlock/CustomBlock.php
@@ -4,7 +4,7 @@ if (! defined('ABSPATH')) exit;
 /*This class initialize the javascript that allows us to download the data we need and an test Alert when entering a post/page editing*/
 class CustomBlock{
     public function __construct(){
-    add_action('enqueue_block_editor_assets', array($this,'loadMyBlockFiles'));
+    do_action('enqueue_block_editor_assets', array($this,'loadMyBlockFiles'));
     }
     /*This initialize and creates the custom block type that will contain our table of users*/
     function loadMyBlockFiles() {
