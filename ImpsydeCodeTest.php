827c073dac0cbc56d994179fff74bd14bbd111c8
Jaleen <jaleenbowenskelly@gmail.com>
2021-12-09 15:10:35 -0500

    Noob Mistake
    NOOB!

--- ImpsydeCodeTest.php
+++ ImpsydeCodeTest.php
@@ -5,7 +5,7 @@
  * Description: This is for a coding test with Impsyde to test my skill in PHP
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Version: 0.6
+ * Version: 0.7
  * Author: Jaleen Bowens-Kelly
  * License: MIT
  */
