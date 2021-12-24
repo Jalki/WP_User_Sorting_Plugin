c0665e7f0676d88a3104faec166af925fe9213d9
Jaleen <jaleenbowenskelly@gmail.com>
2021-12-24 15:48:16 -0500

    OH MY GOD
    I got it! So essentially:
    a) Need a function updateContent(){} to update the block with the content you added/add
    b) edit: Contains our static Content
    c) save: use for styling like color or px

--- src/CustomBlock/block.js
+++ src/CustomBlock/block.js
@@ -3,20 +3,111 @@ wp.blocks.registerBlockType('impsyde/impsyde-table', {
     icon: 'smiley',
     category: 'common',
     attributes: {
-      content: {type: 'string', source: 'text', selector: 'div'},
-      color: {type: 'string'},
-
+      content: {type: 'string'},
+      color: {type: 'string'}
     },
     edit: function(props) {
-      return null
+      function updateContent(event){
+        props.setAttributes({content: event.target.value})
+      }
+
+      function updateContent(value){
+        props.setAttributes({color: value.hex})
+      }
+
+      return wp.element.createElement(
+        "div",
+        null,
+        wp.element.createElement(
+          "table",
+          null,
+          wp.element.createElement(
+            "tr",
+            null,
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "ID"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Name"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Username"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Email"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Address"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Phone"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Website"
+                )
+              ),
+              wp.element.createElement(
+                "td",
+                null,
+                wp.element.createElement(
+                  "h3",
+                  null,
+                  "Company"
+                )
+              )
+
+            )
+            
+          ) 
+      );
     },
     save: function(props) {
       return wp.element.createElement(
-        "div",
-        wp.element.createElement(
-          "h3",
-          "Impsyde Code Test"
-        )
-      );
+        "h3",
+        null,
+        {style: {border: "5px solid" + props.attributes.color} },
+        props.attributes.content
+      )
     }
 })
