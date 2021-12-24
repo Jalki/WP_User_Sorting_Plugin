8ed2a0605fa504009f73130607b97f61092402cf
Jaleen <jaleenbowenskelly@gmail.com>
2021-12-24 13:17:51 -0500

    Learning
    So I know what props does now. It pretty much gather everything from the attributes section of the function. So I need to see if the content sub attribute is the attribute I need to expanded upon to show a text bar

--- src/CustomBlock/block.js
+++ src/CustomBlock/block.js
@@ -1,19 +1,14 @@
 wp.blocks.registerBlockType('impsyde/impsyde-table', {
     title: 'Impsyde-Table-Users',
-    icon: 'table',
+    icon: 'smiley',
     category: 'common',
     attributes: {
-      content: {type: 'string'},
-      color: {type: 'string'}
+      content: {type: 'string', source: 'text', selector: 'div'},
+      color: {type: 'string'},
+
     },
     edit: function(props) {
-      return wp.element.createElement(
-        "div",
-        wp.element.createElement(
-          "h3",
-          "Impsyde Code Test"
-        )
-      );
+      return null
     },
     save: function(props) {
       return wp.element.createElement(
